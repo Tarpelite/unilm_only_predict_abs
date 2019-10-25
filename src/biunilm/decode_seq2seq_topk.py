fbb11d9175ec758896bad6b29c54fa1414f99381
Tarpelite <tarpelite_chan@foxmail.com>
2019-10-25 11:13:27 +0800

    fix bugs

--- src/biunilm/decode_seq2seq_topk.py
+++ src/biunilm/decode_seq2seq_topk.py
@@ -292,7 +292,7 @@ def main():
         
         # assert len(output_lines) == len(all_prekeyphrase_results)
 
-        with open(fn_out, "wb") as fout:
+        with open(fn_out, "w+") as fout:
             for l in (output_lines):
                 fout.write('\t'.join(l))
                 fout.write("\n")
